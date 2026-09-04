18d0347fd4353a3e00d56ed702f167bcbd7b5f80
dev-chelo <angelparedestorres.apt@gmail.com>
2026-09-03 18:58:43 -0600

    feat: se agrega nueva pieza al juego

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ const COLORS = [
   '#e57373', // Z - red
   '#90caf9', // J - pale blue
   '#ffb74d', // L - orange
+  '#b0bec5', // N - gris (tuerca)
 ];
 
 const PIECES = [
@@ -24,7 +25,10 @@ const PIECES = [
   [[5,5,0],[0,5,5],[0,0,0]],                  // Z
   [[6,0,0],[6,6,6],[0,0,0]],                  // J
   [[0,0,7],[7,7,7],[0,0,0]],                  // L
+  [[8,8,8],[8,0,8],[8,8,8]],                  // N - tuerca (3x3 con hueco central)
 ];
+
+const PIECE_COUNT = PIECES.length - 1;
 
 const LINE_SCORES = [0, 100, 300, 500, 800];
 
@@ -52,7 +56,7 @@ function createBoard() {
 }
 
 function randomPiece() {
-  const type = Math.floor(Math.random() * 7) + 1;
+  const type = Math.floor(Math.random() * PIECE_COUNT) + 1;
   const shape = PIECES[type].map(row => [...row]);
   return { type, shape, x: Math.floor(COLS / 2) - Math.floor(shape[0].length / 2), y: 0 };
 }
